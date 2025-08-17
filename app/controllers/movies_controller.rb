bc2d51cf75b60194cd83040bb9392c0eba3c81d2
amcgrail3 <111305582+amcgrail3@users.noreply.github.com>
2025-08-17 17:43:05 +0000

    correct order of movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,6 @@
 class MoviesController < ApplicationController
   def index
-    @list_of_movies = Movie.all
+    @list_of_movies = Movie.all.order({ :year => :desc, :title => :asc })
     
     render({ :template => "movie_templates/index" })
   end
